cf30e32ccbf295f54e2ce1b09133ac196a170245
Dezolutions <mars92851@gmail.com>
2021-08-06 20:51:32 +0300

    added start template

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios  from 'axios'
+import {DebounceInput} from 'react-debounce-input'
+const App = () => {
+  const [value, setValue] = React.useState('')
 
-function App() {
+  React.useEffect(() => {
+    const fetchUser = async () => {
+
+    }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Github Searcher</h1>
+      <DebounceInput
+        debounceTimeout={300}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
